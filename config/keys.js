02febd8d69374c4febcad6d122b14be0953ed630
raghav7203 <raghavsharma7203@gmail.com>
2021-01-12 02:13:47 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://raghav123:raghav123@cluster0.xzdwg.mongodb.net/raghav123?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Item = require('../../models/Item');
+
+// @route GET api/items
+//  @desc GET all items
+// @access Public
+router.get('/', (req, res) => {
+    Item.find()
+     .sort({ date: -1 }) // sort data in descending(-1) order of date
+     .then(items => res.json(items))
+});
+
+// @route POST api/items
+//  @desc Create An Item
+// @access Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+// @route DELETE api/items/:id
+//  @desc Delete An Item
+// @access Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+     .then(item => item.remove().then(() => res.json({ success: true})))
+     .catch(err => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
